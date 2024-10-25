3dcf3f2a1d9711f78ddf930e5a891518fd36ffd4
Varshini <166305910+varshini-0506@users.noreply.github.com>
2024-10-25 22:20:57 +0530

    added profile_url

--- app.py
+++ app.py
@@ -48,6 +48,7 @@ except Exception as e:
 @app.route('/profile', methods=['GET'])
 def get_profile():
     username = request.args.get('username')  # Get username from query parameters
+    profile_pic_url = get_profile_picture(username)
 
     if not username:
         return jsonify({'error': 'Username is required'}), 400
@@ -68,6 +69,7 @@ def get_profile():
             'following': profile.following_count,
             'posts': profile.media_count,
             'is_business': profile.is_business,
+            'profile_url':profile_pic_url,
         }
 
         # Initialize fields
